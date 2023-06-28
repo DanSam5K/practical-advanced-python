b0f3bf73b2ae4ed8ae3c646a6d3d3508ca585ce4
Daniel Samuel <dansam5000@gmail.com>
2023-06-28 12:33:32 +0000

    solve challenge 1

--- runstone-projects/chapter1/circuit_gate.py
+++ runstone-projects/chapter1/circuit_gate.py
@@ -1,5 +1,4 @@
 class LogicGate:
-
     def __init__(self, lbl):
         self.name = lbl
         self.output = None
@@ -142,21 +141,14 @@ class Connector:
         return self.to_gate
 
 def main():
-    # g1 = AndGate("G1")
-    # g2 = AndGate("G2")
-    # g3 = OrGate("G3")
-    # g4 = NotGate("G4")
-    # c1 = Connector(g1, g3)
-    # c2 = Connector(g2, g3)
-    # c3 = Connector(g3, g4)
-    g1 = NorGate("G1")
+    g1 = AndGate("G1")
+    g2 = AndGate("G2")
+    g3 = OrGate("G3")
+    g4 = NotGate("G4")
+    c1 = Connector(g1, g3)
+    c2 = Connector(g2, g3)
+    c3 = Connector(g3, g4)
     print(g1.get_output())
 
 
 main()
-
-
-
-
-
-
