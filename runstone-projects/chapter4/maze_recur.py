857c5e1d2c7de884a824ca60c760db75637b4b81
Daniel Samuel <dansam5000@gmail.com>
2023-08-09 17:53:29 +0000

    - Implement tower of honoi recursive solution - Build recur maze class for exploring maze a s file

---
+++ runstone-projects/chapter4/maze_recur.py
@@ -0,0 +1,98 @@
+import turtle
+
+START= "S"
+OBSTACLE = "+"
+TRIED = "."
+DEAD_END = "_"
+PART_OF_PATH = "O"
+
+class Maze:
+    def __init__(self, maze_filename):
+        with open(maze_filename, "r") as maze_file:
+            self.maze_list = [
+                [ch for ch in line.rstrip("\n")] for line in maze_file.readlines()
+            ]
+        self.rows_in_maze = len(self.maze_list)
+        self.columns_in_maze = len(self.maze_list[0])
+        for row_idx, row in enumerate(self.maze_list):
+            if START in row:
+                self.start_row = row_idx
+                self.start_col = row.index(START)
+                break
+        
+        self.x_translate = -self.columns_in_maze / 2
+        self.y_translate = self.rows_in_maze / 2
+        self.t = turtle.Turtle()
+        self.t.shape("turtle")
+        self.wn = turtle.Screen()
+        self.wn.setworldcoordinates(
+            -(self.columns_in_maze - 1) / 2 - 0.5,
+            -(self.rows_in_maze - 1) / 2 - 0.5,
+            (self.columns_in_maze - 1) / 2 + 0.5,
+            (self.rows_in_maze - 1) / 2 + 0.5,
+        )
+
+    def draw_maze(self):
+        self.t.speed(10)
+        self.wn.tracer(0)
+        for y in range(self.rows_in_maze):
+            for x in range(self.columns_in_maze):
+                if self.maze_list[y][x] == OBSTACLE:
+                    self.draw_centered_box(
+                        x + self.x_translate, -y + self.y_translate, "orange"
+                    )
+        self.t.color("black")
+        self.t.fillcolor("blue")
+        self.wn.update()
+        self.wn.tracer(1)
+
+    def draw_centered_box(self, x, y, color):
+        self.t.up()
+        self.t.goto(x - 0.5, y - 0.5)
+        self.t.color(color)
+        self.t.fillcolor(color)
+        self.t.setheading(90)
+        self.t.down()
+        self.t.begin_fill()
+        for i in range(4):
+            self.t.forward(1)
+            self.t.right(90)
+        self.t.end_fill()
+        
+
+    def update_position(self, row, col, val=None):
+        if val:
+            self.maze_list[row][col] = val
+        self.move_turtle(col, row)
+
+        if val == PART_OF_PATH:
+            color = "green"
+        elif val == OBSTACLE:
+            color = "red"
+        elif val == TRIED:
+            color = "black"
+        elif val == DEAD_END:
+            color = "red"
+        else:
+            color = None
+
+        if color:
+            self.drop_bread_crumb(color)
+
+    def move_turtle(self, x, y):
+        self.t.up()
+        self.t.setheading(self.t.towards(x + self.x_translate, -y + self.y_translate))
+        self.t.goto(x + self.x_translate, -y + self.y_translate)
+
+    def drop_bread_crumb(self, color)
+        self.t.dot(10, color)
+
+    def is_exit(self, row, col):
+        return(
+            row == 0 or row == self.rows_in_maze - 1 or col == 0 or col == self.columns_in_maze - 1
+        )
+        
+    def __getitem__(self, idx):
+        return self.maze_list[idx]
+    
+    
