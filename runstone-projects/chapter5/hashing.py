9bf72d40ea4ad6baab648b0930576955c8eb980b
Daniel Samuel <dansam5000@gmail.com>
2023-09-15 15:22:58 +0200

    Put item into the hash table

--- runstone-projects/chapter5/hashing.py
+++ runstone-projects/chapter5/hashing.py
@@ -1,4 +1,30 @@
 #@Hashing functions for use with a hash table
-def hash_str(a_str, table_size):
-    return sum([(ord(c)*(i+1)) for i, c in enumerate(a_str)]) % table_size
+
+class HashTable:
+    def __init__(self, size):
+        self.size = size
+        self.slots = [None] * self.size
+        self.data = [None] * self.size
+
+    def put(self, key, data):
+        hash_value = self.hash_function(key, len(self.slots))
+
+        if self.slots[hash_value] is None:
+            self.slots[hash_value] = key
+            self.data[hash_value] = data
+        else:
+            if self.slots[hash_value] == key:
+                self.data[hash_value] = data
+            else:
+                next_slot = self.rehash(hash_value, len(self.slots))
+
+                while self.slots[next_slot] is not None and \
+                        self.slots[next_slot] != key:
+                    next_slot = self.rehash(next_slot, len(self.slots))
+
+                if self.slots[next_slot] is None:
+                    self.slots[next_slot] = key
+                    self.data[next_slot] = data
+                else:
+                    self.data[next_slot] = data
 
