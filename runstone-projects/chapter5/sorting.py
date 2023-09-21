e5dfed4799114ba6e35b60876c4ddb385893fdde
Daniel Samuel <dansam5000@gmail.com>
2023-09-21 04:26:02 +0200

    selection sort algorithm

--- runstone-projects/chapter5/sorting.py
+++ runstone-projects/chapter5/sorting.py
@@ -1,29 +1,45 @@
-def bubble_sort(a_list):
-    for i in range(len(a_list)-1, 0, -1):
-        for j in range(i):
-            if a_list[j] > a_list[j+1]:
-                temp = a_list[j]
-                a_list[j] = a_list[j+1]
-                a_list[j+1] = temp
-                # a_list[j], a_list[j+1] = a_list[j+1], a_list[j]  # This is the same as the above 3 lines
-    return a_list
+class Sorting:
+    def __init__(self):
+        pass
+
+    def bubble_sort(self, a_list):
+        for i in range(len(a_list)-1, 0, -1):
+            for j in range(i):
+                if a_list[j] > a_list[j+1]:
+                    temp = a_list[j]
+                    a_list[j] = a_list[j+1]
+                    a_list[j+1] = temp
+                    # a_list[j], a_list[j+1] = a_list[j+1], a_list[j]  # This is the same as the above 3 lines
+        return a_list
 
 
+    def bubble_sort_short(self, a_list):
+        for i in range(len(a_list)-1, 0, -1):
+            exchanges = False
+            for j in range(i):
+                if a_list[j] > a_list[j+1]:
+                    exchanges = True
+                    a_list[j], a_list[j+1] = a_list[j+1], a_list[j]
+            if not exchanges:
+                break
+        return a_list
 
-def bubble_sort_short(a_list):
-    for i in range(len(a_list)-1, 0, -1):
-        exchanges = False
-        for j in range(i):
-            if a_list[j] > a_list[j+1]:
-                exchanges = True
-                a_list[j], a_list[j+1] = a_list[j+1], a_list[j]
-        if not exchanges:
-            break
-    return a_list
+    def selection_sort(self, a_list):
+        for i, item in enumerate(a_list):
+            min_index = len(a_list) - 1
+            for j in range(i, len(a_list)):
+                if a_list[j] < a_list[min_index]:
+                    min_index = j
+            if min_index != i:
+                a_list[i], a_list[min_index] = a_list[min_index], a_list[i]
+        return a_list
 
 
 a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-print(bubble_sort(a_list))
+sort_data = Sorting()
+print(sort_data.bubble_sort(a_list))
+print(sort_data.bubble_sort_short(a_list))
+print(sort_data.selection_sort(a_list))
 # temp = a_list[0]
 # print(temp, a_list[0])
 # a_list[0] = a_list[1]
