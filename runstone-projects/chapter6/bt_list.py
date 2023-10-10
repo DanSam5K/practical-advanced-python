4c971ab40303ebb085dc42d7834aea35364b6e44
Daniel Samuel <dansam5000@gmail.com>
2023-10-10 03:56:35 +0200

    Get and set values

--- runstone-projects/chapter6/bt_list.py
+++ runstone-projects/chapter6/bt_list.py
@@ -20,3 +20,25 @@ def insert_right(root, new_branch):
     return root
 
 
+def get_root_val(root):
+    return root[0]
+
+
+def set_root_val(root, new_val):
+    root[0] = new_val
+
+
+def get_left_child(root):
+    return root[1]
+
+
+def get_right_child(root):
+    return root[2]
+
+# my_tree = ["a", ["b", ["d", [], []], ["e", [], []]], ["c", ["f", [], []], []]]
+# print(my_tree)
+# print("left subtree = ", my_tree[1])
+# print("root = ", my_tree[0])
+# print("right subtree = ", my_tree[2])
+# print("left subtree root = ", my_tree[1][0])
+
