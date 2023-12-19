d8a81978de97a00398427e7b8be0422880896743
Daniel Samuel <dansam5000@gmail.com>
2023-12-19 12:56:26 +0000

    creat a tree and insert obj

--- runstone-projects/chapter6/nodes_ref.py
+++ runstone-projects/chapter6/nodes_ref.py
@@ -32,3 +32,15 @@ class BinaryTree():
     def get_right_child(self):
         return self.right_child
     
+    
+a_tree = BinaryTree("a")
+print(a_tree.get_root_val())
+print(a_tree.get_left_child())
+a_tree.insert_left("b")
+print(a_tree.get_left_child())
+print(a_tree.get_left_child().get_root_val())
+a_tree.insert_right("c")
+print(a_tree.get_right_child().get_root_val())
+a_tree.get_right_child().set_root_val("Hello")
+print(a_tree.get_right_child().get_root_val())
+    
